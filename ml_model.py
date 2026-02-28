2eda0c49bb7471041c56faa6c53e30a00a60cbdd
narain08051984 <narayanmenon06@gmail.com>
2026-02-28 23:49:16 +0530

    added three new ml model code

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,13 @@
 
 
 print("adding all ml related code here")
+
+
+def model(1):
+    print("adding code for ml model 1")
+
+def model(2):
+    print("adding code for ml model 2")
+
+def model(3):
+    print("adding code for ml model 2")    
